cdf85cd81b0af2868731772dbe2f9bb3c45083a0
Kathryn McKean <KMCKEAN907@GMAIL.COM>
2023-04-25 19:09:07 -0500

    adds a thank you and hidden heal btn

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 let counter = 0;
 
+const relieveWaluigiOfHisPain = () => {
+    const waluigiTexts = document.querySelectorAll(".WaluigiText");
+    console.log(waluigiTexts)
+    // if waluigiTexts is truthy remove the h2s
+    if (waluigiTexts.length > 0) {
+        for (const item of waluigiTexts) {
+            item.remove();
+        }
+        document.getElementById("waluigiPic").src = "https://static.miraheze.org/greatcharacterswiki/4/46/Waluigi.png";
+        console.log("you have laid waluigi to rest... he thanks you");
+        counter = 0;
+
+        const thanksContainer = document.querySelector(".WaluigiJail");
+        const thankYou = document.createElement("h2");
+        thankYou.setAttribute("class", "thankYou");
+        thankYou.textContent = "<3 :)  wahhh  :) <3";
+        thanksContainer.appendChild(thankYou);
+    }
+};
+
+
+
 const onWaluigiClick = () => {
-    console.log("waaaah you clicked me!");
+    const hasbtn = document.querySelector('.header').querySelector('.FunButton');
+
+    if (hasbtn == null) {
+        const btnContainer = document.querySelector('.header')
+        const btn = document.createElement("button");
+        btn.setAttribute('class', 'FunButton');
+        btn.textContent = "click here to heal waluigi"
+        btn.addEventListener('click', relieveWaluigiOfHisPain);
+        console.log("waaaah you clicked me!");
+        btnContainer.appendChild(btn);
+    }
+
     // if theres 11 wahhhs then return
-    if (counter >= 11) {
+
+
+    if (counter >= 10) {
         return
     }
     const waluigiContainer = document.querySelector(".wahMargin");
@@ -21,18 +56,3 @@ const onWaluigiClick = () => {
     document.getElementById("waluigiPic").src = "https://external-preview.redd.it/3RPB3isFSOx1G-RCny4ABHBGbrr9vOrpeIsgsL3QvJs.png?auto=webp&s=ea2050723da6d99075796c1f57275031c671aade";
 
 }
-
-const relieveWaluigiOfHisPain = () => {
-    const waluigiTexts = document.querySelectorAll(".WaluigiText");
-    console.log(waluigiTexts)
-    // if waluigiTexts is truthy remove the h2s
-    if (waluigiTexts.length > 0) {
-        for (const item of waluigiTexts) {
-            item.remove();
-        }
-        document.getElementById("waluigiPic").src = "https://static.miraheze.org/greatcharacterswiki/4/46/Waluigi.png";
-        console.log("you have laid waluigi to rest... he thanks you");
-        counter = 0;
-
-    }
-};
